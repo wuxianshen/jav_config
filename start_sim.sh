9d6ec22fcb444eda5c387cca710da8961dc7f301
tao.jing <wuxianshen@163.com>
2019-03-21 11:08:00 +0800

    Init version of jav config files

---
+++ start_sim.sh
@@ -0,0 +1,1 @@
+/home/taojing/share/JAV/Onboard-SDK/utility/bin/linux/x86-64/M210ConfigTool --usb-port /dev/ttyACM0 --config-file UserConfig.txt --simulation on --latitude 39.106933 --longitude 117.168405
---
+++ stop_sim.sh
@@ -0,0 +1,1 @@
+/home/taojing/share/JAV/Onboard-SDK/utility/bin/linux/x86-64/M210ConfigTool --usb-port /dev/ttyACM0 --config-file UserConfig.txt --simulation off --latitude 37.422083 --longitude -122.137390
